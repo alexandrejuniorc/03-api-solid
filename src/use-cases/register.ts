120577e8a06ba852bd50cae30dd22e332426cf70
Alexandre Junior <alexandrejunior408@gmail.com>
2023-03-31 16:52:48 -0300

    chore: repository pattern

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseProps {
@@ -12,7 +13,7 @@ export async function registerUseCase({
   email,
   password,
 }: RegisterUseCaseProps) {
-  const passwordHash = await hash(password, 6)
+  const password_hash = await hash(password, 6)
 
   const userWithSameEmail = await prisma.user.findUnique({
     where: {
@@ -24,11 +25,11 @@ export async function registerUseCase({
     throw new Error('E-mail already exists.')
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: passwordHash,
-    },
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
   })
 }
