82195118bdfbe535f7c2bc2be773c21b87500912
Alexandre Junior <alexandrejunior408@gmail.com>
2023-04-06 10:26:08 -0300

    chore: validando distância do check-in

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -43,11 +43,6 @@ describe("CheckIn Use Case", () => {
     expect(checkIn.id).toEqual(expect.any(String));
   });
 
-  // methodology TDD
-  // state red => error in test
-  // state green => success in test
-  // refactor => refactor test
-
   it("should be able to check-in twice in the same day", async () => {
     vi.setSystemTime(new Date(2023, 4, 5, 8, 0, 0));
 
@@ -88,5 +83,25 @@ describe("CheckIn Use Case", () => {
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to check-in on distant gym", async () => {
+    gymsRepository.items.push({
+      id: "gym-02",
+      title: "Node Academy",
+      description: "",
+      phone: "",
+      latitude: new Decimal(-27.0747279),
+      longitude: new Decimal(-49.4889672),
+    });
+
+    await expect(() =>
+      sut.execute({
+        gymId: "gym-01",
+        userId: "user-01",
+        userLatitude: -27.2092052,
+        userLongitude: -49.6401091,
+      })
+    ).rejects.toBeInstanceOf(Error)
   });
 });
