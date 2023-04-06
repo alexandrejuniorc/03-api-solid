28b04002586d3beb3e38cea01082eb3de47524ab
Alexandre Junior <alexandrejunior408@gmail.com>
2023-04-06 17:10:40 -0300

    chore: caso de uso de busca de academias

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -19,15 +19,21 @@ export class InMemoryGymsRepository implements GymsRepository {
     const gym = {
       id: data.id ?? randomUUID(),
       title: data.title,
-      description: data.description ??  null,
+      description: data.description ?? null,
       phone: data.phone ?? null,
-      latitude: new Prisma.Decimal(data.latitude.toString()) ,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
       longitude: new Prisma.Decimal(data.longitude.toString()),
-      created_at: new Date()
-    }
+      created_at: new Date(),
+    };
 
-    this.items.push(gym)
+    this.items.push(gym);
 
-    return gym
+    return gym;
+  }
+
+  async searchMany(query: string, page: number) {
+    return this.items
+      .filter((item) => item.title.includes(query))
+      .slice((page - 1) * 20, page * 20);
   }
 }
---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,28 @@
+import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import { Gym } from "@prisma/client";
+import { GymsRepository } from "@/repositories/gyms-repository";
+
+interface SearchGymsUseCaseProps {
+ query: string
+ page: number
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[];
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+   query,
+   page
+  }: SearchGymsUseCaseProps): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page);
+
+    return {
+      gyms,
+    };
+  }
+}
