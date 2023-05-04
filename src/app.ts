1a629962c7802e4902a9f2873e620e1c30f84dd3
Alexandre Junior <alexandrejunior408@gmail.com>
2023-05-04 14:52:14 -0300

    chore: testes e2e de rotas de academias

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,17 @@ import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
+import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 
 export const app = fastify();
 
 app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
-})
+  secret: env.JWT_SECRET,
+});
 
-app.register(usersRoutes)
-app.register(gymsRoutes)
+app.register(usersRoutes);
+app.register(gymsRoutes);
+app.register(checkInsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
@@ -21,9 +23,9 @@ app.setErrorHandler((error, _, reply) => {
       .send({ message: "Validation error.", issues: error.format });
   }
 
-  if(env.NODE_ENV !== 'production'){
-    console.error(error)
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
   }
 
-  return reply.status(500).send({message: 'Internal server error.'})
+  return reply.status(500).send({ message: "Internal server error." });
 });
