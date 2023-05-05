ba292768f8c8d1285bafd2be607254138af5b303
Alexandre Junior <alexandrejunior408@gmail.com>
2023-05-05 16:08:51 -0300

    chore: autorização por cargos (RBAC)

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -5,13 +5,18 @@ import { create } from "./create";
 import { validate } from "./validate";
 import { history } from "./history";
 import { metrics } from "./metrics";
+import { verifyUserRole } from "@/middlewares/verify-user-role";
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
-  app.get('/check-ins/history', history)
-  app.get('/check-ins/metrics', metrics)
+  app.get("/check-ins/history", history);
+  app.get("/check-ins/metrics", metrics);
 
-  app.post('/gyms/:gymId/check-ins', create)
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.post("/gyms/:gymId/check-ins", create);
+  app.patch(
+    "/check-ins/:checkInId/validate",
+    { onRequest: [verifyUserRole("ADMIN")] },
+    validate
+  );
 }
